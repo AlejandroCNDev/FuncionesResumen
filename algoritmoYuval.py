502b6085d2269e2b37fe1ac693bd5ea17847f8db
ALEJANDRO CASTRO NAVARRO <Alejandro.Castro3@alu.uclm.es>
2022-12-29 15:24:00 +0100

    Versión Funcional: Requiere cambios y mejoras.

--- algoritmoYuval.py
+++ algoritmoYuval.py
@@ -1,24 +1,30 @@
-import string,random,time,zlib,hashlib,numpy as np
+import hashlib
+import numpy as np
+import random
+import string
+import time
+import zlib
 
-class funcionesResumen:
+
+class algoritmoYuval:
 
     # __init__ is a special method called whenever you try to make
     # an instance of a class. As you heard, it initializes the object.
     # Here, we'll initialize some of the data.
-    def __init__(self, algorithm, numexperiments, bits):
+    def __init__(self, algorithm, bits):
         # Let's add some data to the [instance of the] class.
         self.algorithmHash = algorithm
         self.bits = bits
-        self.numexperiments = numexperiments
 
     def generateHash(self, algorithm, bytesMessage, numBits):
-        # import hashlib (to apply SHA,MD5,...)
+        # we import hashlib (to apply SHA,MD5,...) and zlib to use crc32
+
         # md5 = haslib.md5(b'Hola mundo')
         # print(md5.hexdigest())
-        generatedHash = ""
+
         if algorithm == "crc32":
             generatedHash = zlib.crc32(bytesMessage)
-            #generatedHash = generatedHash[:int(numBits / 4)]
+            # generatedHash = generatedHash[:int(numBits / 4)]
         elif algorithm == "md5":
             generatedHash = hashlib.md5(bytesMessage).hexdigest()
             generatedHash = generatedHash[:int(numBits / 4)]
@@ -30,7 +36,8 @@ class funcionesResumen:
 
         return generatedHash
 
-    def generateNewPassword(self,x):
+    #ESTO LO HE HECHO YO PA QUE FUNCIONE PERO NO SE DEBEN HACER ASI LAS MODIFICACIONES
+    def generateNewPassword(self, x):
         # The characters to generate password (only ascii_lowercase)
         # characters = list(string.ascii_letters + string.digits + "@!$#&%^()*")
 
@@ -50,6 +57,7 @@ class funcionesResumen:
         # Returning the random password.
         return newpassword
 
+    # Modificaciones menores del mensaje ilegitimo
     def modificacionMenorMi(self):
 
         mil1 = ["Hello ", "Hi "]
@@ -75,15 +83,19 @@ class funcionesResumen:
 
         return combinations_mi
 
-
+    # Modificaciones menores del mensaje legitimo
     def modificacionMenoresMl(self):
 
         ml1 = ["Hello, ", "Good Morning, "]
-        ml2 = ["It is my pleasure to contact you today to inform you of the opportunity we have for you. ", "It is a privilege to contact you to let you know about the incredible opportunity we have for you. "]
-        ml3 = ["You have proven to be a reliable person for us, ", "You have reaffirmed that you are a person we can trust, "]
-        ml4 = ["and I know that you will be able to help us make this innovative project a success. ", "and I know that you will be able to make our future project a success. "]
+        ml2 = ["It is my pleasure to contact you today to inform you of the opportunity we have for you. ",
+               "It is a privilege to contact you to let you know about the incredible opportunity we have for you. "]
+        ml3 = ["You have proven to be a reliable person for us, ",
+               "You have reaffirmed that you are a person we can trust, "]
+        ml4 = ["and I know that you will be able to help us make this innovative project a success. ",
+               "and I know that you will be able to make our future project a success. "]
         ml5 = ["I am writing to you ", "I am sending you this email  "]
-        ml6 = ["because I have recently become aware of your experience and expertise. ", "because I have just recently become aware of your experience, skills and knowledge. "]
+        ml6 = ["because I have recently become aware of your experience and expertise. ",
+               "because I have just recently become aware of your experience, skills and knowledge. "]
         ml7 = ["If they are correct, ", "If they are true,  "]
         ml8 = ["I would like to offer you ", "I would like to propose you "]
         ml9 = ["the job in our company. ", "the possibility of a job in our company. "]
@@ -95,11 +107,12 @@ class funcionesResumen:
         ml15 = ["and send it to me. ", "and send it to me at this same e-mail."]
         ml16 = ["Regards.", "Best Regards."]
 
-        combinations_ml = np.array(np.meshgrid(ml1, ml2, ml3, ml4, ml5, ml6, ml7, ml8, ml9, ml10, ml11, ml12, ml13, ml14, ml15, ml16)).T.reshape(-1, 16)
+        combinations_ml = np.array(
+            np.meshgrid(ml1, ml2, ml3, ml4, ml5, ml6, ml7, ml8, ml9, ml10, ml11, ml12, ml13, ml14, ml15,
+                        ml16)).T.reshape(-1, 16)
 
         return combinations_ml
 
-    """
     def algoritmoYuval(self):
 
         # Para calcular el tiempo de ejecución
@@ -112,86 +125,34 @@ class funcionesResumen:
         for i in combinations_ml:
             str_ml = "".join(i)
             bytes_ml = bytes(str_ml, encoding='utf-8')
-            h = h1= self.generateHash(self.algorithmHash,bytes_ml,self.bits)
+            h = self.generateHash(self.algorithmHash, bytes_ml, self.bits)
             table_hash[h] = "".join(i)
 
         nocollision = True
-        while nocollision: # 3: While no se encuentre colisión do {probable en t intentos}
-            x = self.modificacionMenorMi() # 4: Generar x′i modificación menor de xi y computar h(x′i)
+        # 3: While no se encuentre colisión do {probable en t intentos}
+        while nocollision:
+            # 4: Generar x′i modificación menor de xi y computar h(x′i)
+            x = self.modificacionMenorMi()
             for i in x:
                 str_mi = "".join(i)
-                #print(str_mi)
-                bytes_mi=bytes(str_mi + self.generateNewPassword(100), encoding='utf-8')
-                h1= self.generateHash(self.algorithmHash,bytes_mi,self.bits)
+                bytes_mi = bytes(str_mi + self.generateNewPassword(100), encoding='utf-8')
+                h1 = self.generateHash(self.algorithmHash, bytes_mi, self.bits)
 
-                if h1 in table_hash: # 5: Buscar si existe x′l tal que h(x′l) = h(x′i)
+                # 5: Buscar si existe x′l tal que h(x′l) = h(x′i)
+                if h1 in table_hash:
                     print("Existió una colision: \n" + "El mensaje es: " + str(bytes_mi))
                     print("Su hash es: " + str(h1))
-                    nocollision=False
+                    nocollision = False
                     print("El mensaje legitimo con el mismo hash que el ilegitimo es: \n")
                     print(table_hash.get(h1))
                     print("Y el mensaje ilegitimo es: " + str_mi)
-                    break
 
+                    fin_time = time.time()
+                    print("El tíempo de ejecución del programa " + str(fin_time - ini_time))
+
+                    return nocollision
 
         print("No se encontró colisión.")
         fin_time = time.time()
         print("El tíempo de ejecución del programa " + str(fin_time-ini_time))
-"""
-
-
-if __name__ == "__main__":
-
-    ## PREAMBULO DE LO DE LA CONSOLA DE LOS EXPERIMENTOS
-
-    f = funcionesResumen()
-
-    #for i in range(NUMERO DE EXPERIMENTOS):
-
-    # Para calcular el tiempo de ejecución
-    ini_time = time.time()
-    # 1: Se generan  t = 2^(m/2) modificaciones menores de xl
-    combinations_ml = f.modificacionMenoresMl()
-
-    # 2: Computar el resumen y almacenar los pares (x′l, h(x′l))
-    table_hash = {}
-    for i in combinations_ml:
-        str_ml = "".join(i)
-        bytes_ml = bytes(str_ml, encoding='utf-8')
-        h = h1 = f.generateHash(f.algorithmHash, bytes_ml, self.bits)
-        table_hash[h] = "".join(i)
-
-    nocollision = True
-    while nocollision:  # 3: While no se encuentre colisión do {probable en t intentos}
-        x = f.modificacionMenorMi()  # 4: Generar x′i modificación menor de xi y computar h(x′i)
-        for i in x:
-            str_mi = "".join(i)
-            # print(str_mi)
-            bytes_mi = bytes(str_mi + f.generateNewPassword(100), encoding='utf-8')
-            h1 = f.generateHash(f.algorithmHash, bytes_mi, f.bits)
-
-            if h1 in table_hash:  # 5: Buscar si existe x′l tal que h(x′l) = h(x′i)
-                print("Existió una colision: \n" + "El mensaje es: " + str(bytes_mi))
-                print("Su hash es: " + str(h1))
-                nocollision = False
-                print("El mensaje legitimo con el mismo hash que el ilegitimo es: \n")
-                print(table_hash.get(h1))
-                print("Y el mensaje ilegitimo es: " + str_mi)
-                break
-
-    print("No se encontró colisión.")
-    fin_time = time.time()
-    print("El tíempo de ejecución del programa " + str(fin_time - ini_time))
-
-
-    ## FUERA DEL FOR
-    """  
-import numpy as np 
-  
-list = [2, 4, 4, 4, 5, 5, 7, 9] 
-  
-print(np.average(list)) // (np.std(list)) // (np.var(list))
-"""
-    print("El tiempo medio de ejecución")
-
-
+        return nocollision
--- experiments.py
+++ experiments.py
@@ -1,6 +1,9 @@
 
 import getopt,sys
 
+from algoritmoYuval import *
+
+
 def help():
     print("Diplaying Help. Options available")
     print("python {*.py} -h --Help  => Display Help ")
@@ -10,60 +13,85 @@ def help():
     print("Obligatory to use the following arguments: -anb")
 
 
-#def main():
-argumentList = sys.argv[1:]
+def main():
 
-# Options (anb requires an argument, thats why it has :)
-options = "ha:n:b:"
+    argumentList = sys.argv[1:]
 
-# Long options : 'period=' requires an argument, so it has = suffixed
-long_options = ["help", "Help", "Algorithm=","algorithm=", "Experiments=", "experiments=", "Bits Hash= ", "bits=", "bitsHash=",""]
+    # Options (anb requires an argument, thats why it has :)
+    options = "ha:n:b:"
 
-try:
+    # Long options : 'period=' requires an argument, so it has = suffixed
+    long_options = ["help", "Help", "Algorithm=","algorithm=", "Experiments=", "experiments=", "Bits Hash= ", "bits=", "bitsHash=",""]
 
-    if (len(sys.argv) == 1):
-       help()
-       raise ValueError("Se requieren más argumentos")
+    try:
 
-    arguments, values = getopt.getopt(argumentList, options, long_options)
+        if (len(sys.argv) == 1):
+           help()
+           raise ValueError("Se requieren más argumentos")
 
-    # list of options tuple (opt, value)
-    print('Options Tuple is {}'.format(arguments))
+        arguments, values = getopt.getopt(argumentList, options, long_options)
 
-    # list of remaining command-line arguments
-    print('Additional Command-line arguments list is {}'.format(values))
+        # list of options tuple (opt, value)
+        print('Options Tuple is {}'.format(arguments))
 
-    # Checking each argument
-    for currentArgument, currentValue in arguments:
-        currentArgument = currentArgument.lower()  # Converting all options to lowercase
-        print(currentArgument)
+        # list of remaining command-line arguments
+        print('Additional Command-line arguments list is {}'.format(values))
 
-        if currentArgument in ("-h", "--help"):
-            help()
-        elif currentArgument in ("-a", "--algorithm"):
-            algorithm = currentValue.lower()
-            if algorithm == "crc32":
-                    print("You can become a web developer.")
-            elif algorithm == "sha":
-                    print("You can become a Data Scientist")
-            elif algorithm == "md5":
-                    print("You can become a backend developer")
-            else:
-                    raise ValueError("The algorithm was not found. (only is valid crc32,md5,sha)")
-            print("Algorithm used:", algorithm)
-        elif currentArgument in ("-n", "--numberexperiments"):
-            if not currentValue.isnumeric():
-                raise TypeError('Work with Positive Numbers Only')
-            print(("Number of experiments to do: (% s)") % (currentValue))
+        # Checking each argument
+        for currentArgument, currentValue in arguments:
+            currentArgument = currentArgument.lower()  # Converting all options to lowercase
+            print(currentArgument)
 
-        elif currentArgument in ("-b", "--bitshash"):
-            if not currentValue.isnumeric():
-                raise TypeError('Work with Numbers Only')
-            print(("Number of Bits of Hash to use: (% s)") % (currentValue))
+            if currentArgument in ("-h", "--help"):
+                help()
+            elif currentArgument in ("-a", "--algorithm"):
+                algorithm = currentValue.lower()
+                if algorithm == "crc32":
+                        print("You can become a web developer.")
+                elif algorithm == "sha":
+                        print("You can become a Data Scientist")
+                elif algorithm == "md5":
+                        print("You can become a backend developer")
+                else:
+                        raise ValueError("The algorithm was not found. (only is valid crc32,md5,sha)")
+                print("Algorithm used:", algorithm)
+            elif currentArgument in ("-n", "--numberexperiments"):
+                if not currentValue.isnumeric():
+                    raise TypeError('Work with Positive Numbers Only')
+                print(("Number of experiments to do: (% s)") % (currentValue))
+                numberexperiments = currentValue
 
-except getopt.error as err:
-    print(str(err))
-    help()
+            elif currentArgument in ("-b", "--bitshash"):
+                if not currentValue.isnumeric():
+                    raise TypeError('Work with Numbers Only')
+                print(("Number of Bits of Hash to use: (% s)") % (currentValue))
+                bitshash = currentValue
 
-#if __name__ == "__main__":
- #   main()
+
+    except getopt.error as err:
+        print(str(err))
+        help()
+
+    successfull_experiments = {}
+    times_experiments = []
+
+    for i in range(int(numberexperiments)):
+        # Para calcular el tiempo de ejecución
+        ini_time = time.time()
+        test = algoritmoYuval(algorithm, bitshash)
+        ok = test.algoritmoYuval()
+        successfull_experiments[i] = "".join("ok")
+        fin_time = time.time()
+        timeOneExperiment = fin_time - ini_time
+        times_experiments.append(timeOneExperiment)
+        print("El tíempo de ejecución del programa " + str(timeOneExperiment))
+
+
+    print("La media obtenida de los experimentos es: " + str(np.average(times_experiments)))
+    print("La varianza obtenida de los experimentos es: " + str(np.var(times_experiments)))
+    print("La desviación tipica obtenida de los experimentos es: " + str(np.std(times_experiments)))
+
+    print(successfull_experiments)
+
+if __name__ == "__main__":
+    main()
